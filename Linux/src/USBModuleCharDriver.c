e44bb8a3b0b62cbcbdb6849e70ccaf75da36263e
Roger Pease <rogerpease@gmail.com>
2025-06-02 19:24:08 -0500

    Tweaks and moved my_class into main structure.

--- Linux/src/USBModuleCharDriver.c
+++ Linux/src/USBModuleCharDriver.c
@@ -19,10 +19,11 @@
 //    Make sure your Arduino GUI sees your Xiao device and its COM or /dev/tty port. 
 //    File -> Examples -> AdaFruit TinyUSB Library (may be towards bottom) -> Vendor -> i2c_tiny_usb_adapter
 //    Flash the device. 
-//    Connect the device to your linux host. You may need to plug/unplug it. 
-//    insmod usbmon 
-//    cat /sys/kernel/debug/usb/usbmon/3u 
-//    % i2cdetect /dev/i2c-0        should scan the I2C bus of the Xiao (might try connecting an LCD1602 to give it something to connect to). 
+//    Connect the device to your linux host. 
+// 
+//    linux% insmod usbmon 
+//    linux% cat /sys/kernel/debug/usb/usbmon/3u 
+//    linux% i2cdetect /dev/i2c-0        should scan the I2C bus of the Xiao (might try connecting an LCD1602 to give it something to connect to). 
 //    From the trace from above you should see lines like  
 
 // ffff9e00c0fb4180 1657276607 S Ci:3:008:0 s c1 01 0000 0000 0004 4 <
@@ -39,21 +40,20 @@
 //    In the i2c-tiny-usb Arduino program, after i2c_usb.begin() in setup() add:    TinyUSBDevice.setID(0xaaaa,0x0b0b);
 //
 //    Recompile and reflash. You may need to short the bootloader pins to force the Xiao into bootloader mode.
+// 
 //    Blacklist the i2c-tiny-usb driver with a file in /etc/modprobe.d 
+//    linux% make clean; make 
+//    linux% insmod USBModuleCharDriver 
+//    Plug USB device in (or remove/plug in)
+//    linux% lsusb           (and verify the aaaa:0b0b appears). 
+//    linux% Test.py. 
 //
-//    % make clean; make 
-//    % insmod USBModuleCharDriver. 
-//    Replug the i2c device in. Run 
-//    % lsusb           (and verify the aaaa:0b0b appears). 
-//    Run Test.py. 
-//
-
-
-//     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
-//00:                         08 -- 0a -- -- 0d -- --
 
 
 #include <linux/usb.h> 
+
+#define DRIVER_NAME "USBModuleCharDriver "
+#define DEVICE_NAME "simpleusb"
 
 #define USB_SKEL_VENDOR_ID  (0xaaaa)
 #define USB_SKEL_PRODUCT_ID (0x0b0b)
@@ -62,7 +62,8 @@
 #define RESULT_BUFFER_LEN  (256)
 
 //
-// In a "real" implementation I would of course build an array or linked list of these so multiple users could manage their own i2c modules. 
+// In a "real" implementation I would of course build an array or linked list of these so 
+// multiple users could manage their own i2c queries. 
 // This would be worth doing for a shared resource like a GPU.  
 // 
 
@@ -74,7 +75,7 @@ typedef struct {
 
   char command[COMMAND_LEN];  
 
-  char resultBuffer[RESULT_BUFFER_LEN];  
+  char resultBuffer[RESULT_BUFFER_LEN];
   int resultBeginIndex; 
   int resultEndIndex; 
 
@@ -85,14 +86,13 @@ typedef struct {
 typedef struct { 
 
     struct usb_device *dev; 
-
     char probed; 
-    int char_driver_setup; 
-
     int fh_open; 
-
     spinlock_t lock; 
-    struct cdev my_cdev;
+
+    struct class *my_class;
+    int major_number; 
+    struct device *my_device;
 
 } my_usb_device_t; 
 
@@ -120,15 +120,18 @@ MODULE_DEVICE_TABLE (usb, my_usb_table);
 
 int issue_urb(char *,MyFHPrivateData_t *); 
 
+//
+// Gets called once per *interface* when a USB device is plugged in. 
+//
 static int my_usb_probe(struct usb_interface *interface, const struct usb_device_id *id) 
 {
     if ((id->idVendor  == USB_SKEL_VENDOR_ID) && 
         (id->idProduct == USB_SKEL_PRODUCT_ID)) 
     { 
 
-        if (!my_usb_device->char_driver_setup) { 
+        if (my_usb_device->my_device == NULL) { 
             if (setup_char_driver() < 0)  
-                pr_info("Could not set up char driver.");  
+                pr_err("Could not set up char driver.");  
          }
 
         struct usb_host_interface *iface_desc;
@@ -168,14 +171,13 @@ static int my_usb_probe(struct usb_interface *interface, const struct usb_device
  
 }
 
+// Gets called when user application calls os.open
 
 static int charDriverFileOpen(struct inode *inode, struct file *filp)
 {
     int count = 0; 
 
-    pr_info("Open called");  
-    if (my_usb_device->fh_open) { pr_err("Already open"); return -ENOSPC; } 
-    pr_info("Will open File");  
+    if (my_usb_device->fh_open) { pr_err(DRIVER_NAME "Device Already open"); return -ENOSPC; } 
 
     my_usb_device->fh_open = 1; 
 
@@ -188,11 +190,12 @@ static int charDriverFileOpen(struct inode *inode, struct file *filp)
     myFHPrivateData->resultEndIndex = 0; 
 
     filp->private_data = (void *) myFHPrivateData; 
-    pr_info("Opening File");  
   
     return 0;
 
 }
+
+// Gets called when user application calls os.close 
 
 static int charDriverFileClose(struct inode *inode, struct file *filp)
 {
@@ -202,6 +205,8 @@ static int charDriverFileClose(struct inode *inode, struct file *filp)
     return 0;
 
 }
+
+// Gets called when user application calls os.read() from a system device they opened. 
 
 ssize_t charDriverFileRead(struct file *filp, char * buf, size_t byteCount, loff_t * byteOffset)
 {
@@ -222,10 +227,13 @@ ssize_t charDriverFileRead(struct file *filp, char * buf, size_t byteCount, loff
     return bytesWrittenThisCall; 
 
 }
-//
-// Issue a command (in the form of a line from a usbmon transcript). 
-//
-ssize_t charDriverFileWrite (struct file *filp, const char * buf, size_t byteCount, loff_t * byteOffset)
+
+// Gets called when user application calls os.write(). 
+// This is where we issue commands to the device. 
+// byteOffset is essentially a 'reminder' pointer in case a write happens at multiple points. 
+
+ssize_t charDriverFileWrite (struct file *filp, 
+                             const char * buf, size_t byteCount, loff_t * byteOffset)
 {
 
     MyFHPrivateData_t * myFHPrivateData = filp->private_data;
@@ -245,6 +253,8 @@ ssize_t charDriverFileWrite (struct file *filp, const char * buf, size_t byteCou
 
 }
 
+// Table of things to do when the *character* device is open. 
+
 static const struct file_operations my_cdev_fops = {
 	.owner          = THIS_MODULE,
         .write          = charDriverFileWrite,  
@@ -254,69 +264,64 @@ static const struct file_operations my_cdev_fops = {
 
 };
 
-#define DEVICE_NAME "simpleusb"
-
-static struct class *my_class;
-static struct device *my_device;
-static int major_number; 
-
 
 int setup_char_driver() {
 
-   pr_info("Setup Char driver"); 
+   // Allocate a major device number. Otherwise everyone would use 228 or 240. 
 
-   dev_t dev;
-
-   major_number = register_chrdev(0, DEVICE_NAME, &my_cdev_fops);
-   if (major_number < 0)
+   pr_err(DRIVER_NAME "Setting up char device"); 
+   my_usb_device->major_number = register_chrdev(0, DEVICE_NAME, &my_cdev_fops);
+   if (my_usb_device->major_number < 0)
    {
        pr_err("Failed to register char device"); 
-       return major_number; 
+       return my_usb_device->major_number; 
    }
 
-   pr_info("Major device %d",major_number); 
+   // Allocate a class so you can see the driver's information in /sys/class/DEVICE_NAME. 
 
-   my_class = class_create(DEVICE_NAME);
+   my_usb_device->my_class = class_create(DEVICE_NAME);
 
-   if (IS_ERR(my_class))
+   if (IS_ERR(my_usb_device->my_class))
    {
-       unregister_chrdev(major_number,DEVICE_NAME); 
-       pr_err("Failed to create class"); 
-       return PTR_ERR(my_class);  
+       unregister_chrdev(my_usb_device->major_number,DEVICE_NAME); 
+       pr_err(DRIVER_NAME "Failed to create class"); 
+       return PTR_ERR(my_usb_device->my_class);  
    } 
 
+   // Now create the actual minor device and entry in /dev. 
+   pr_info(DRIVER_NAME"Creating device"); 
 
-   my_device = device_create(my_class, NULL, MKDEV(major_number,0), NULL, DEVICE_NAME);
+   my_usb_device->my_device = device_create(my_usb_device->my_class, 
+                         NULL, MKDEV(my_usb_device->major_number,0), NULL, DEVICE_NAME);
 
-   if (IS_ERR(my_device)) {
-        class_destroy(my_class);
-        unregister_chrdev(major_number, DEVICE_NAME);
+   if (IS_ERR(my_usb_device->my_device)) {
+        class_destroy(my_usb_device->my_class);
+        my_usb_device->my_class = NULL;
+        unregister_chrdev(my_usb_device->major_number, DEVICE_NAME);
         printk(KERN_ALERT "Failed to create device\n");
-        return PTR_ERR(my_device);
+        return PTR_ERR(my_usb_device->my_device);
    }
-
-   my_usb_device->char_driver_setup = 1; 
 
    return 0;
 } 
 
 void destroy_char_driver() {
 
-   if (!my_usb_device->char_driver_setup)  return; 
+   if (my_usb_device->my_class == NULL) return; 
+   
+   device_destroy   (my_usb_device->my_class,MKDEV(my_usb_device->major_number,0)); 
 
-   device_destroy(my_class,MKDEV(major_number,0)); 
-   class_destroy(my_class);
-   unregister_chrdev(major_number, DEVICE_NAME);
+   class_destroy    (my_usb_device->my_class);
 
-   my_usb_device->char_driver_setup = 0; 
+   unregister_chrdev(my_usb_device->major_number, DEVICE_NAME);
+
+   my_usb_device->my_class = NULL; 
 
 }
 
 static void my_usb_disconnect(struct usb_interface *interface)
 {
     destroy_char_driver(); 
-    int minor = interface->minor; 
-    pr_info("Interface disconnected %d",minor); 
 }
 
 static struct usb_driver skel_driver = {
@@ -335,7 +340,7 @@ int issue_urb(char * usbmonline,MyFHPrivateData_t *myFHPrivateData)
 {
 
     char * data; 
-    if (!my_usb_device->probed) { pr_err("Not registered"); return 0; }
+    if (!my_usb_device->probed) { pr_err(DRIVER_NAME "Not registered"); return 0; }
 
     char sendCallBack = usbmonline[0]; 
     char urbType      = usbmonline[2]; 
@@ -352,7 +357,7 @@ int issue_urb(char * usbmonline,MyFHPrivateData_t *myFHPrivateData)
     int requestlength = 0;
     int endpoint      = 0; 
 
-//                            v Send starting here 
+//                            v Write starting here 
 //                              
 //ffff9e00c0fb4180 1657276607 S Ci:3:008:0 s c1 01 0000 0000 0004 4 <
 //ffff9e00c0fb4180 1657277850 C Ci:3:008:0 0 4 = 0100ff8e
@@ -360,32 +365,30 @@ int issue_urb(char * usbmonline,MyFHPrivateData_t *myFHPrivateData)
 
     if ((sendCallBack == 'S') && (urbType == 'C'))
     {
-        pr_info("scanf calling"); 
         // https://docs.kernel.org/usb/usbmon.html
         sscanf(usbmonline+2,"%c%c:%d:%d:%d "
                             "%c %x %d %d %d %d",
                                   &urbignore[0],&urbignore[1],&bus,&device,&endpoint,
                                   &setup,&requesttype,&request,&requestvalue,&requestindex,&requestlength); 
-        pr_info("scanf called urbignore %c%c",urbignore[0],urbignore[1]); 
     }
     else
-        pr_err("Unimplemented %s",usbmonline);
+        pr_err(DRIVER_NAME " Unimplemented %s",usbmonline);
     
     // usb_control_msg(struct usb_device *dev, unsigned int pipe, __u8 request,
     //     __u8 requesttype, __u16 value, __u16 index, void *data,
     //     __u16 size, int timeout)
 
-    pr_info("%c %c%c ep%d req:%d reqtype:%x requestvalue:%d rindex:%d",sendCallBack,urbType,urbDir,endpoint,request,requesttype,requestvalue,requestindex); 
+    pr_debug(DRIVER_NAME "%c %c%c ep%d req:%d reqtype:%x requestvalue:%d rindex:%d",sendCallBack,urbType,urbDir,endpoint,request,requesttype,requestvalue,requestindex); 
     
     int status; 
     int pipe;
 
     if      ((urbType == 'C') && (urbDir == 'i')) pipe = usb_rcvctrlpipe(my_usb_device->dev,endpoint); 
     else if ((urbType == 'C') && (urbDir == 'o')) pipe = usb_sndctrlpipe(my_usb_device->dev,endpoint); 
-    else { pr_err("unknown urb and command %c %c: {r|w} c ",urbType,urbDir); return 1; }  
+    else { pr_err(DRIVER_NAME "unknown urb and command %c %c: {r|w} c ",urbType,urbDir); return 1; }  
 
     data = kzalloc(64,GFP_KERNEL);
-    if (! data) pr_err("TRY URBs data not allocated"); 
+    if (!data) pr_err(DRIVER_NAME "TRY URBs data not allocated"); 
 
     status = usb_control_msg(my_usb_device->dev, pipe, request,  requesttype, requestvalue, requestindex,data,requestlength,2000); 
     
@@ -414,7 +417,6 @@ int issue_urb(char * usbmonline,MyFHPrivateData_t *myFHPrivateData)
 
 static int __init my_module_init(void)
 {
-    pr_info("USBModule: my_module_init called "); 
 
     // Really not sure why but kzalloc is not zeroing-out things. 
     my_usb_device = (my_usb_device_t *) kmalloc(sizeof(my_usb_device),GFP_KERNEL); 
@@ -426,14 +428,13 @@ static int __init my_module_init(void)
     } 
 
     my_usb_device->fh_open           = 0; 
-    my_usb_device->char_driver_setup = 0; 
     my_usb_device->probed            = 0; 
+    my_usb_device->my_class          = NULL; 
+    my_usb_device->my_device         = NULL; 
 
     spin_lock_init(&my_usb_device->lock);
 
     int result = usb_register(&skel_driver);
- 
-
 
     if (result < 0) {
         pr_err("usb_register failed for the %s driver. Error number %d\n",
@@ -451,10 +452,9 @@ static int __init my_module_init(void)
 static void __exit my_module_exit(void)
 {
 
-   if (my_usb_device->char_driver_setup) 
+   if (my_usb_device->my_class != NULL) 
        destroy_char_driver();
 
-   pr_info("Deregistering "); 
    usb_deregister(&skel_driver);
 
    kfree(my_usb_device);
